5a2cf38654510ec923c26231a9794fd75ee6c8c2
is2js <tingstyle1@gmail.com>
2024-07-24 10:52:10 +0900

    feat: hero x-data 도입 및 스크롤 처리

--- js/hero.js
+++ js/hero.js
@@ -1,10 +1,15 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.data("timer", (year, month, day, hours, minutes) => ({
+    Alpine.data("hero", (year, month, day, hours, minutes) => ({
         expiry: null,
         remaining: null,
         init() {
-            // Ensure expiryDate is a Date object
+            // 스크롤 방지 by root요소를 top/left로 onscroll마다 이동
+            this.disableScroll();
+
+            // 한국시간으로 만료기한 넣기
             this.expiry = this.toKoreanTime(new Date(year, month - 1, day, hours, minutes)).getTime();
+
+            // 남은시간 한번 넣고, 1초마다 다시 계산
             this.setRemaining();
             setInterval(() => {
                 this.setRemaining();
@@ -52,6 +57,33 @@ document.addEventListener('alpine:init', () => {
                 minutes: this.format(this.minutes().value),
                 seconds: this.format(this.seconds().value),
             };
+        },
+        disableScroll() {
+            let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+            /* 1) 스크롤 움직일 시, 복귀 (천천히) */
+            window.onscroll = function () {
+                window.scrollTo(scrollTop, scrollLeft);
+            };
+
+            /* 2) 스크롤 움직임에 대해, 칼같이 움직여서 사실상 안움직이는 것으로 추가 옵션*/
+            const rootElement = document.querySelector(':root');
+            rootElement.style.scrollBehavior = 'auto';
+        },
+        enableScroll() {
+            window.onscroll = function () {
+
+            };
+
+            const rootElement = document.querySelector(':root');
+            rootElement.style.scrollBehavior = 'smooth';
+
+            /* 4) 브라우저 db에 opened 상태 기록해놓기 */
+            // localStorage.setItem('opened', 'true');
+
+            // 6) 보러가기 클릭 -> enableScroll -> 내부에서 audio도 작동
+            // playAudio();
         }
     }));
 });
